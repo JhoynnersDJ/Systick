d8cb27e00dce7dc0ec703c6d9120f1a94d6f98ba
Heraldsito <richardael14@gmail.com>
2023-08-07 15:50:36 -0400

    Implementacion de side bar

--- Systik/systik-front/src/App.jsx
+++ Systik/systik-front/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Sidebar from './components/Sidebar.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className='bg-color'> Epale Menor</h1>
-    </>
+    <div className=''>
+      <Sidebar />
+    </div>
+
   )
 }
 
