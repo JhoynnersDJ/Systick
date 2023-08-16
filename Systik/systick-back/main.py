685ffddcd78a184c2a9b1736b36b60f6f21e3591
JhoynnersDJ <jhoynners.santaella15@gmail.com>
2023-08-15 23:43:52 -0400

    Agregando la funcionalidad de insertar usuario (Falta acomodar conexion a la base de datos)

--- Systik/systick-back/main.py
+++ Systik/systick-back/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request, Form, HTTPException
 from pydantic import BaseModel
-from typing import Optional
 import mysql.connector
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -10,57 +9,100 @@ templates = Jinja2Templates(directory="templates")
 
 # Configuración de la base de datos (reemplaza con tu configuración)
 db_config = {
-    "host": "192.168.1.50",
-    "user": "DesaBytes",
-    "password": "Melon24%",
+    "host": "localhost",
+    "user": "root",
+    "password": "",
     "database": "systick"
 }
 
 class Usuario(BaseModel):
     nombre_us: str
-    apellido_us: str
+    apelldo_us: str
     correo_us: str
     password: str
     cargo: str
     departamento: str
-    dir_hogar: Optional[str]
-    num_telefono: Optional[str]
-    fech_nac: Optional[str]
+    dir_hogar: str = None
+    num_telefono: str = None
+    fech_nac: str = None
 
-#-----------LOGIN---------------------------------------
 @app.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-#------------------REGISTRO----------------------------------------------
 @app.get('/registro/', response_class=HTMLResponse)
 async def register_page(request: Request):
     return templates.TemplateResponse("registro.html", {"request": request})
 
 @app.post("/registro/", response_class=HTMLResponse)
-async def register(usuario: Usuario, request: Request):
-    try:
+async def register_page(
+    nombre_us: str = Form(...),
+    apellido_us: str = Form(...),
+    correo_us: str = Form(...),
+    password: str = Form(...),
+    repeat_password: str = Form(...),  # Agregado
+    cargo: str = Form(...),
+    departamento: str = Form(...),
+    dir_hogar: str = Form(None),
+    num_telefono: str = Form(None),
+    fech_nac: str = Form(None),
+    request: Request = None
+):
+    if password != repeat_password:  # Validación de contraseñas iguales
+        return templates.TemplateResponse("registro.html", {"request": request, "error_message": "Las contraseñas no coinciden"})
 
+    conn = None
+    cursor = None
+    try:
         # Conectar a la base de datos
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
 
         # Verificar si el correo ya está registrado
-        query = "SELECT * FROM usuarios WHERE correo_us = %s"
-        cursor.execute(query, (usuario.correo_us,))
+        query = "SELECT * FROM usuario WHERE correo_us = %s"
+        cursor.execute(query, (correo_us,))
         existing_usuario = cursor.fetchone()
         if existing_usuario:
             raise HTTPException(status_code=400, detail="Correo ya registrado")
 
         # Insertar el nuevo usuario en la base de datos
-        query = "INSERT INTO usuarios (nombre_us, correo_us, password, cargo, departamento, apellido_us, dir_hogar, fech_nac, num_telefono) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (usuario.nombre_us, usuario.correo_us, usuario.password, usuario.cargo, usuario.departamento, usuario.apellido_us, usuario.dir_hogar, usuario.fech_nac, usuario.num_telefono)
+        query = "INSERT INTO usuario(nombre_us, correo_us, password, cargo, departamento, apellido_us, dir_hogar, fech_nac, num_telefono) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (nombre_us, correo_us, password, cargo, departamento, apellido_us, dir_hogar, fech_nac, num_telefono)
         cursor.execute(query, values)
         conn.commit()
 
         return templates.TemplateResponse("registro.html", {"request": request, "message": "Registro exitoso"})
     except mysql.connector.Error as err:
+        # Agregar mensaje de registro para la excepción
+        print(f"Error en la base de datos: {err}")
         return templates.TemplateResponse("registro.html", {"request": request, "error_message": f"Error en la base de datos: {err}"})
     finally:
-        cursor.close()
-        conn.close()
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+ 
+@app.get('/roles/', response_class=HTMLResponse)
+async def roles_page(request: Request):
+    conn = None
+    cursor = None
+    try:
+        # Conectar a la base de datos
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        # Realizar la consulta SELECT a la tabla roles
+        query = "SELECT * FROM roles"
+        cursor.execute(query)
+        roles = cursor.fetchall()
+
+        return templates.TemplateResponse("roles.html", {"request": request, "roles": roles})
+    except mysql.connector.Error as err:
+        error_message = f"Error en la base de datos: {err}"
+        return templates.TemplateResponse("roles.html", {"request": request, "error_message": error_message})
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
