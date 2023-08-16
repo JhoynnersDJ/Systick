b12b4f44ad9994680259b04222f4782e8360c58e
Heraldsito <richardael14@gmail.com>
2023-08-16 08:58:22 -0400

    Cambiar Nombre de la carpeta

--- Systik/systick-front/src/components/Home/Home.jsx
+++ Systik/systick-front/src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Sidebar from '../Sidebar.jsx';
+import Sidebar2 from '../Sidebar2.jsx';
 
 function Home() {
   return (
     <div>
-      <Sidebar />
+      <Sidebar2 />
 
       <div className="p-4 sm:ml-64">
    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
